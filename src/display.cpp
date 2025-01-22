f67d940129d16bd0ab3c4c07de560ab94f334fae
Philip Lundrigan <lundrigan@byu.edu>
2025-01-21 19:29:02 -0700

    Switch to use Y-board display

--- src/display.cpp
+++ src/display.cpp
@@ -1,33 +1,23 @@
 #include "display.h"
 
-Adafruit_SSD1306 display;
-
-bool setup_display(int address)
-{
-    if (!display.begin(SSD1306_SWITCHCAPVCC, address)) {
-        return false;
-    } 
-    display.clearDisplay();
-    display.setTextColor(1);
-    display.setRotation(0); // Can be 0, 90, 180, or 270
-    display.setTextWrap(true);
-    display.display();
+bool setup_display() {
+    Yboard.display.clearDisplay();
+    Yboard.display.setTextColor(1);
+    Yboard.display.setRotation(0); // Can be 0, 90, 180, or 270
+    Yboard.display.setTextWrap(true);
+    Yboard.display.display();
     return true;
 }
 
-void display_text(const std::string &text, const int text_size)
-{
-    display.setTextSize(text_size);
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.print(text.c_str());
-    display.display();  
+void display_text(const std::string &text, const int text_size) {
+    Yboard.display.setTextSize(text_size);
+    Yboard.display.clearDisplay();
+    Yboard.display.setCursor(0, 0);
+    Yboard.display.print(text.c_str());
+    Yboard.display.display();
 }
 
-void clear_display()
-{
-    display.clearDisplay();
-    display.display();
+void clear_display() {
+    Yboard.display.clearDisplay();
+    Yboard.display.display();
 }
-
-
